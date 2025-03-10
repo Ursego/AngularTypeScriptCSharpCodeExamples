8f6f8bd42c9c89ddec39d17c4b8f0976021c8974
Ursego <90226705+Ursego@users.noreply.github.com>
2025-03-10 10:17:18 -0400

    Add files via upload

---
+++ 2 .Net - MT/Ias.DataChangeSubscriptions.Admin.Domain/DataChgSubServiceAccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ias.DataChangeSubscriptions.Admin.Domain
+{
+    public class DataChgSubServiceAccountDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ 2 .Net - MT/Ias.DataChangeSubscriptions.Admin.Domain/DataChgSubTblDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tyler.Ias.DataChangeSubscriptions.Admin.Domain
+{
+    public class DataChgSubTblDto
+    {
+        public int DataChgSubTblId { get; set; }
+        public int DataChgSubId { get; set; }
+        public string TblName { get; set; }
+        public string ApiPropertyName { get; set; }
+        public string Who { get; set; }
+        public DateTime? Wen { get; set; }
+        public int? ColsCount { get; set; }
+    }
+}
